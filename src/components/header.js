c0320c844769a4c65af209275c7244d5dab8aa02
conradtrost <ct.conradtrost@gmail.com>
2021-09-20 10:29:50 -0500

    Tablet styles added

--- src/components/header.js
+++ src/components/header.js
@@ -7,10 +7,10 @@ import { FaSoundcloud, FaSpotify, FaInstagram, FaYoutube } from "react-icons/fa"
 const Header = () => {
     return(
         <header>
-            <div className="w-full p-4 bg-gradient-to-r to-red-200 via-white from-white absolute top-0 left-0 flex justify-between items-center">
-                <h3 className="w-2/12 text-3xl font-extrabold">MICX</h3>
-                <StaticImage className="w-14" src="../icons/MICX_LOGO.png" />
-                <div className="w-2/12 justify-center items-center hidden md:flex">
+            <div className="w-full p-2 px-4 bg-gradient-to-r to-red-200 via-white from-white absolute top-0 left-0 flex justify-between items-center">
+                <h3 className="w-4/12 text-3xl font-extrabold">MICX</h3>
+                <div className="w-2/12 text-center"><StaticImage className="w-14" src="../icons/MICX_LOGO.png" /></div>
+                <div className="w-4/12 justify-end items-center hidden md:flex">
                     <a rel="noopener noreferrer" target="_blank" href="#soundcloud"><FaSoundcloud /></a>
                     <a rel="noopener noreferrer" target="_blank" href="https://open.spotify.com/artist/6IrRgdLJO148S9gQ4FysZI?si=B1dNi-CESAaxeFmiAblO2w&dl_branch=1"><FaSpotify /></a>
                     <a rel="noopener noreferrer" target="_blank" href="#instagram"><FaInstagram /></a>
--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,7 @@ const IndexPage = () => {
 
     <div className="md:flex justify-center flex-col min-h-full p-8">
       <Player />
-      <div className="lower md:flex md:w-7/12 mx-auto justify-between my-6">
+      <div className="lower md:flex md:w-10/12 lg:w-9/12 xl:w-7/12 mx-auto justify-between my-6">
         <About />
         <Socials />
       </div>
