fe35e71840a14ce7eba26f47294cb35549e8b3e2
conradtrost <ct.conradtrost@gmail.com>
2021-09-21 11:13:01 -0500

    bug fix remove StaticImage header

--- src/components/about.js
+++ src/components/about.js
@@ -6,7 +6,7 @@ const About = () => {
             <div className="p-4 bg-gradient-to-r from-gray-800 to-gray-600 text-center text-white text-2xl">
              <h3>About MICX</h3>
             </div>
-            <div className="bg-white h-52 inner-card flex p-4">
+            <div className="bg-white md:h-52 inner-card flex p-4">
                 <p className="m-auto">MICX is a singer, songwriter, and audio engineer currently operating in Dallas, Texas. Studied under audio engineer Rick Rooney at Empire Sound Studio, learning the ins and outs of recording, mixing, and providing production/post-production services to clients in various genres.</p>
             </div>
         </div>
--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { StaticImage } from 'gatsby-plugin-image';
 
 import { FaSoundcloud, FaSpotify, FaInstagram, FaYoutube } from "react-icons/fa";
+
+import micxLogo from '../icons/MICX_LOGO.png';
 
 
 const Header = () => {
@@ -10,7 +11,7 @@ const Header = () => {
         <header>
             <div className="w-full p-2 px-4 bg-gradient-to-r to-red-200 via-white from-white absolute top-0 left-0 flex justify-between items-center">
                 <h3 className="w-4/12 text-3xl font-extrabold">MICX</h3>
-                <div className="w-2/12 text-center"><StaticImage placeholder="tracedSVG" className="w-14" src="../icons/MICX_LOGO.png" /></div>
+                <div className="w-2/12 text-center"><img alt="" className="w-14 md:m-auto" src={micxLogo} /></div>
                 <div className="w-4/12 justify-end items-center hidden md:flex">
                     <a rel="noopener noreferrer" target="_blank" href="https://soundcloud.app.goo.gl/u3ynsqKtWW5PsCm27"><FaSoundcloud /></a>
                     <a rel="noopener noreferrer" target="_blank" href="https://open.spotify.com/artist/6IrRgdLJO148S9gQ4FysZI?si=B1dNi-CESAaxeFmiAblO2w&dl_branch=1"><FaSpotify /></a>
